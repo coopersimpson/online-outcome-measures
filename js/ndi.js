3cf93b7b7ed60342200e3a05f6d0d0d7b1fe4ac6
Cooper Simpson <cooper@coopersimpson.com>
2023-12-24 14:06:26 +1000

    Two working sections

--- js/ndi.js
+++ js/ndi.js
@@ -1,24 +1,30 @@
 const { PDFDocument } = PDFLib
 
-    async function fillForm() {
-    	// Fetch the PDF with form fields
-      const formUrl = '../pdfs/neck-disability-index.pdf'
-      const formPdfBytes = await fetch(formUrl).then(res => res.arrayBuffer())
+async function fillForm() {
+  // Fetch the PDF with form fields
+  const formUrl = '../pdfs/neck-disability-index.pdf'
+  const formPdfBytes = await fetch(formUrl).then(res => res.arrayBuffer())
 
-      // Load a PDF with form fields
-      const pdfDoc = await PDFDocument.load(formPdfBytes)
+  // Load a PDF with form fields
+  const pdfDoc = await PDFDocument.load(formPdfBytes)
 
-      // Get the form containing all the fields
-      const form = pdfDoc.getForm()
+  // Get the form containing all the fields
+  const form = pdfDoc.getForm()
 
-      // Check the appropriate checkbox based on the selected radio buttons
-      const selectedRadioButtonId = document.querySelector('input[name="pain-intensity"]:checked').id;
-      const checkField = form.getCheckBox(selectedRadioButtonId);
-      checkField.check();
+  // Check the appropriate checkbox based on the selected radio buttons
+  // Pain Intensity
+  const painIntensityRadioButtonId = document.querySelector('input[name="pain-intensity"]:checked').id;
+  const checkPainIntensity = form.getCheckBox(painIntensityRadioButtonId);
+  checkPainIntensity.check();
 
-      // Serialize the PDFDocument to bytes (a Uint8Array)
-      const pdfBytes = await pdfDoc.save()
+  // Personal Care
+  const personalCareRadioButtonId = document.querySelector('input[name="personal-care"]:checked').id;
+  const checkPersonalCare = form.getCheckBox(personalCareRadioButtonId);
+  checkPersonalCare.check();
 
-			// Trigger the browser to download the PDF document
-      download(pdfBytes, "neck-disability-index.pdf", "application/pdf");
-    }
+  // Serialize the PDFDocument to bytes (a Uint8Array)
+  const pdfBytes = await pdfDoc.save()
+
+  // Trigger the browser to download the PDF document
+  download(pdfBytes, "neck-disability-index.pdf", "application/pdf");
+}
